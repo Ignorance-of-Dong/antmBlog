1c17591e45c0c70859609f376923af2eedee3167
Ignorance-of-Dong <1054914660@qq.com>
2019-11-13 16:12:00 +0800

    '打包'

--- outherblog/service-worker.js
+++ outherblog/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c658b04800b8677a0696e7fe4f4ab74.js"
+  "/precache-manifest.31c07d02ff82503ae5ef85204b4f966c.js"
 );
 
 self.addEventListener('message', (event) => {
